94c469a8f19b4f3572a73c9de1cbc95fa774a010
Maxim Fedorov <familiar.hedgehog@gmail.com>
2015-03-03 18:16:24 +0100

    Squash access code into table queries

--- src/main/scala/model.scala
+++ src/main/scala/model.scala
@@ -7,13 +7,6 @@ case class Choice(testId: Int, text: String, right: Boolean)
 
 import scala.slick.driver.JdbcProfile
 
-object DbConnection {
-  import scala.slick.driver.H2Driver
-  lazy val profile = H2Driver
-}
-
-object Schema extends Schema(DbConnection.profile)
-
 class Schema(val profile: JdbcProfile) {
 
   import scala.slick.model.ForeignKeyAction._
@@ -28,7 +21,10 @@ class Schema(val profile: JdbcProfile) {
     def * = (id, subject, question, explanation) <> (Test.tupled, Test.unapply)
   }
 
-  val Tests = TableQuery[Tests]
+  object Tests extends TableQuery(new Tests(_)) {
+    def add(tests: List[Test])(implicit session: Session) = this ++= tests
+    def all(implicit session: Session): List[Test] = this.list
+  }
 
   class Choices(t: Tag) extends Table[Choice](t, "choice") {
     val testId = column[Int]    ("test_id")
@@ -40,25 +36,10 @@ class Schema(val profile: JdbcProfile) {
     def * = (testId, text, right) <> (Choice.tupled, Choice.unapply)
   }
 
-  val Choices = TableQuery[Choices]
+  object Choices extends TableQuery(new Choices(_)) {
+    def add(choices: List[Choice])(implicit session: Session) = this ++= choices
+    def all(implicit session: Session): List[Choice] = this.list
+  }
 
   def create()(implicit s: Session) = Seq(Tests, Choices) foreach (_.ddl.create)
 }
-
-
-import scala.slick.jdbc.JdbcBackend.Session
-import Schema._, profile.simple._
-
-object TestDao {
-  def insert(tests: List[Test])(implicit session: Session) = Tests ++= tests
-
-  def list(implicit session: Session): List[Test] = Tests.list
-}
-
-object ChoiceDao {
-  def insert(choices: List[Choice])(implicit session: Session) = Choices ++= choices
-
-  def list(implicit session: Session): List[Choice] = Choices.list
-}
-
-
--- src/test/scala/TestSpec.scala
+++ src/test/scala/TestSpec.scala
@@ -1,8 +1,8 @@
 package slickways
 
-import scala.slick.jdbc.JdbcBackend.{Database, Session}
+import scala.slick.driver.JdbcProfile
 
-abstract class TestSuite(db: Database) extends org.scalatest.FunSuite {
+abstract class TestSuite(db: JdbcProfile#Backend#Database, profile: JdbcProfile) extends org.scalatest.FunSuite {
   test("H2") {
     val newTests = List(
       Test(
@@ -22,18 +22,23 @@ abstract class TestSuite(db: Database) extends org.scalatest.FunSuite {
       Choice(2, "a measure of belief in some outcome", false)
     )
 
+    val schema = new Schema(profile)
+    import schema.{Tests, Choices}
+
     db withTransaction { implicit s =>
+      schema.create()
+      Tests add newTests
+      assert(Tests.all === newTests)
 
-      Schema.create()
-
-      TestDao insert newTests
-      assert(TestDao.list === newTests)
-
-      ChoiceDao insert newChoices
-      assert(ChoiceDao.list === newChoices)
+      Choices add newChoices
+      assert(Choices.all === newChoices)
     }
   }
 }
 
+import scala.slick.driver.H2Driver
 class H2TestSuite
-  extends TestSuite(Database.forURL("jdbc:h2:mem:test", driver = "org.h2.Driver"))
+  extends TestSuite(
+    H2Driver.simple.Database.forURL("jdbc:h2:mem:test", driver = "org.h2.Driver"),
+    H2Driver
+  )
